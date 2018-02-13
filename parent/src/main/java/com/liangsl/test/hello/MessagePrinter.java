d70b8bd52f8262759f9555084eee6850a4d6d5df
liangshenlong <liangshenlong@foxmail.com>
2018-02-13 15:14:13 +0800

    一些修改

--- parent/src/main/java/com/liangsl/test/hello/MessagePrinter.java
+++ parent/src/main/java/com/liangsl/test/hello/MessagePrinter.java
@@ -1,19 +1,42 @@
 package com.liangsl.test.hello;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MessagePrinter {
-
-    final private MessageService service;
-
+public class MessagePrinter implements InitializingBean, DisposableBean, BeanNameAware, BeanFactoryAware {
     @Autowired
+    private MessageService service;
+
     public MessagePrinter(MessageService service) {
         this.service = service;
     }
 
+    public MessagePrinter(){}
+
     public void printMessage() {
         System.out.println(this.service.getMessage());
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+
+    }
+
+    @Override
+    public void setBeanName(String s) {
+
+    }
+
+    @Override
+    public void destroy() throws Exception {
+
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
     }
 }
