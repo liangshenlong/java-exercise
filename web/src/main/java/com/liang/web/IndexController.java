f9de196dbdd71e7e1fd787fbcbd74a073f76f557
liangshenlong <314267619@qq.com>
2018-02-15 10:42:13 +0800

    使用jstl表达式,jsp页面需加<%@ page isELIgnored="false" %>

--- web/src/main/java/com/liang/web/IndexController.java
+++ web/src/main/java/com/liang/web/IndexController.java
@@ -3,6 +3,7 @@ package com.liang.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,8 +13,9 @@ public class IndexController {
     private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
 
     @RequestMapping("index")
-    public String index(){
+    public String index(ModelMap modelMap){
         logger.info("the first jsp pages");
+        modelMap.put("name","333");
         return "index";
     }
 }
--- web/src/main/webapp/WEB-INF/pages/index.jsp
+++ web/src/main/webapp/WEB-INF/pages/index.jsp
@@ -1,5 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page isELIgnored="false" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
+<head>
+    <title>index</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
 <body>
+${name}
 <h2>Hello World!</h2>
 </body>
 </html>
