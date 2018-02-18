7954b05dc45240ad81ab8642c92b79c2b420658a
liangshenlong <314267619@qq.com>
2018-02-18 19:56:01 +0800

    增加spring bean生命周期实例

---
+++ parent/src/main/java/com/liangsl/test/lifecycle/TestCyclelife.java
@@ -0,0 +1,21 @@
+package com.liangsl.test.lifecycle;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestCyclelife {
+
+    public static void main(String[] args){
+        System.out.println("--------------【初始化容器】---------------");
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        System.out.println("-------------------【容器初始化成功】------------------");
+        //得到studentBean，并显示其信息
+        StudentBean studentBean = context.getBean("studentBean",StudentBean.class);
+        System.out.println(studentBean);
+
+        System.out.println("--------------------【销毁容器】----------------------");
+        ((ClassPathXmlApplicationContext)context).registerShutdownHook();
+    }
+
+}
