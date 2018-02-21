efff83c23fa56f6ab6eaa129a202c95d087fc282
liangshenlong <314267619@qq.com>
2018-02-21 19:43:16 +0800

    add activemq

--- parent/src/main/java/com/liangsl/test/hello/MessagePrinter.java
+++ parent/src/main/java/com/liangsl/test/hello/MessagePrinter.java
@@ -1,12 +1,10 @@
 package com.liangsl.test.hello;
 
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MessagePrinter implements InitializingBean, DisposableBean, BeanNameAware, BeanFactoryAware {
+public class MessagePrinter{
     @Autowired
     private MessageService service;
 
@@ -18,25 +16,5 @@ public class MessagePrinter implements InitializingBean, DisposableBean, BeanNam
 
     public void printMessage() {
         System.out.println(this.service.getMessage());
-    }
-
-    @Override
-    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-
-    }
-
-    @Override
-    public void setBeanName(String s) {
-
-    }
-
-    @Override
-    public void destroy() throws Exception {
-
-    }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-
     }
 }
